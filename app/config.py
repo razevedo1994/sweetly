08b6edfd858540f44ab7318f2627ce7f1fd5c328
Rodrigo Azevedo <razevedo.contato@gmail.com>
2026-06-11 19:28:50 -0300

    feat: define pydantic settings

--- app/config.py
+++ app/config.py
@@ -3,8 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", extra="allow"
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="allow",
     )
+
+    sqlite_file_name: str | None = None
+    echo: bool | None = None
 
 
 settings = Settings()
