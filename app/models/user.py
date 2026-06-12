bdbd6e2ba2c72340df828e38e37feac7633b8687
Rodrigo Azevedo <razevedo.contato@gmail.com>
2026-06-12 08:59:28 -0300

    feat: create user sqlmodel

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from datetime import UTC, datetime
+from typing import Optional
+
+from services.auth import HashedPassword
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, nullable=False)
+    password: HashedPassword
+    role: str
+    is_active: bool = Field(default=True, nullable=False)
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(UTC), nullable=False
+    )
