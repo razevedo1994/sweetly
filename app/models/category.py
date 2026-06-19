da195c912aa09c876feb5575c23660feaac8944d
Rodrigo Azevedo <razevedo.contato@gmail.com>
2026-06-19 07:11:33 -0300

    typo

--- app/models/category.py
+++ app/models/category.py
@@ -4,7 +4,7 @@ from typing import Optional
 from sqlmodel import Field, SQLModel
 
 
-class category(SQLModel):
+class Category(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True, nullable=False)
     created_at: datetime = Field(
