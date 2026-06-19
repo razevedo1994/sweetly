ffc92ae88d4ac103a51f2865cd2f344ec17c3188
Rodrigo Azevedo <razevedo.contato@gmail.com>
2026-06-19 07:46:58 -0300

    feat: create relationship

--- app/models/category.py
+++ app/models/category.py
@@ -1,12 +1,16 @@
 from datetime import UTC, datetime
 from typing import Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
+
+from app.models.product import Product
 
 
-class Category(SQLModel):
+class Category(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True, nullable=False)
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(UTC), nullable=False
     )
+
+    products: list["Product"] = Relationship(back_populates="category")
